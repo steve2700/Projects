21f6d226909f997f407e3d7876d9af527682edb3
steve2700 <stevnyaruwa@gmail.com>
2023-05-19 01:05:50 +0200

    ai

--- api.js
+++ api.js
@@ -18,23 +18,50 @@ var api = {
     });
   }
 };
-
 function displayWeather(weatherData) {
   var div = document.getElementById("weather");
   div.innerHTML = "<h2>" + weatherData.name + "</h2><p>" + weatherData.weather[0].description + "</p><p>" + (weatherData.main.temp - 273.15).toFixed(2) + "°C</p>";
 
+  var dailyTemperatures = [];
+  var daysOfWeek = [];
+
+  // Extract temperature and day of the week data
+  for (var i = 0; i < weatherData.daily.length; i++) {
+    var temperature = (weatherData.daily[i].temp.day - 273.15).toFixed(2);
+    var date = new Date(weatherData.daily[i].dt * 1000);
+    var dayOfWeek = date.toLocaleDateString("en-US", { weekday: "short" });
+
+    dailyTemperatures.push(temperature);
+    daysOfWeek.push(dayOfWeek);
+  }
+
+  var ctx = document.getElementById("weatherChart").getContext("2d");
+  var weatherChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: daysOfWeek,
+      datasets: [
+        {
+          label: "Temperature (°C)",
+          data: dailyTemperatures,
+          backgroundColor: "rgba(0, 136, 204, 0.2)",
+          borderColor: "rgba(0, 136, 204, 1)",
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
   setBackgroundBasedOnWeather(weatherData.weather[0].main);
 }
 
-function setBackgroundBasedOnWeather(weatherCondition) {
-  var backgroundImageUrl = '';
-
-  if (weatherCondition === 'Clear') {
-    backgroundImageUrl = 'https://images.unsplash.com/photo-1601297183305-6df142704ea2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2xlYXIlMjBza3l8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
-  } else if (weatherCondition === 'Rain') {
-    backgroundImageUrl = 'https://media.istockphoto.com/id/1429701799/photo/raindrops-on-asphalt-rain-rainy-weather-downpour.jpg?b=1&s=170667a&w=0&k=20&c=lXXWPQuhXI4xZRrr8d1uZGjQasuR-oRS1_GraXO9Fd0=';
-  } else if (weatherCondition === 'Clouds') {
-    backgroundImageUrl = 'https://media.istockphoto.com/id/1345919662/photo/panoramic-view-of-clear-blue-sky-and-clouds-clouds-with-background.jpg?s=1024x1024&w=is&k=20&c=SqQIXcNNk38ddmgWJIzLbld_I5CzwwU0iVqeM8I87Ts=';
-  } else {
-    backgroundImageUrl = 'https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx'
 

f5db604fde015061effc5cf4e0a5703681cad3e2
steve2700 <stevnyaruwa@gmail.com>
2023-05-18 16:57:06 +0200

    changes

--- api.js
+++ api.js
@@ -1,22 +1,25 @@
 var api = {
   getWeather: function(city) {
     // Make an API request to get the weather for the specified city.
-    var response = $.ajax({
+    $.ajax({
       url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=5ec5845a9b7c8e9451636b9e096d36ab",
       dataType: "json"
+    })
+    .done(function(response) {
+      // Check the response status code.
+      if (response.cod === 200) {
+        // The request was successful.
+        var weatherData = response;
+        console.log(weatherData);
+        displayWeather(weatherData);
+      } else {
+        // The request failed.
+        console.log("Request failed with status code " + response.cod);
+      }
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.log("Request failed: " + textStatus + ", " + errorThrown);
     });
-
-    // Check the response status code.
-    if (response.status === 200) {
-      // The request was successful.
-      var weatherData = response;
-
-      console.log(weatherData);
-      displayWeather(weatherData);
-    } else {
-      // The request failed.
-      console.log("Request failed with status code " + response.status);
-    }
   }
 };
 

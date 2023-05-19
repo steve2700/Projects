e100113de9ef6a57b9074b0917695caa983e5c87
steve2700 <stevnyaruwa@gmail.com>
2023-05-19 11:31:16 +0200

    woow

--- api.js
+++ api.js
@@ -20,55 +20,32 @@ var api = {
 };
 
 function displayWeather(weatherData) {
-  var div = document.getElementById("weather");
-  div.innerHTML = "<h2>" + weatherData.name + "</h2><p>" + weatherData.weather[0].description + "</p><p>" + (weatherData.main.temp - 273.15).toFixed(2) + "°C</p>";
+  var weatherContainer = document.getElementById("weather");
+  
+  // Clear the previous content
+  weatherContainer.innerHTML = "";
+  
+  // Create elements for each weather data point
+  var cityName = document.createElement("h2");
+  cityName.textContent = weatherData.name;
 
-  var dailyTemperatures = [];
-  var daysOfWeek = [];
+  var weatherDescription = document.createElement("p");
+  weatherDescription.textContent = weatherData.weather[0].description;
 
-  // Extract temperature and day of the week data
-  for (var i = 0; i < weatherData.daily.length; i++) {
-    var temperature = (weatherData.daily[i].temp.day - 273.15).toFixed(2);
-    var date = new Date(weatherData.daily[i].dt * 1000);
-    var dayOfWeek = date.toLocaleDateString("en-US", { weekday: "short" });
+  var temperature = document.createElement("p");
+  temperature.textContent = "Temperature: " + (weatherData.main.temp - 273.15).toFixed(2) + "°C";
 
-    dailyTemperatures.push(temperature);
-    daysOfWeek.push(dayOfWeek);
-  }
-
-  var ctx = document.getElementById("weatherChart").getContext("2d");
-  var weatherChart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: daysOfWeek,
-      datasets: [
-        {
-          label: "Temperature (°C)",
-          data: dailyTemperatures,
-          backgroundColor: "rgba(0, 136, 204, 0.2)",
-          borderColor: "rgba(0, 136, 204, 1)",
-          borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-
-  setBackgroundBasedOnWeather(weatherData.weather[0].main);
+  // Append the elements to the weather container
+  weatherContainer.appendChild(cityName);
+  weatherContainer.appendChild(weatherDescription);
+  weatherContainer.appendChild(temperature);
+  
+  // Add custom styling to the weather container
+  weatherContainer.style.border = "1px solid #ccc";
+  weatherContainer.style.padding = "10px";
+  weatherContainer.style.borderRadius = "8px";
+  weatherContainer.style.marginTop = "20px";
+  weatherContainer.style.backgroundColor = "#f5f5f5";
+  weatherContainer.style.textAlign = "center";
 }
-
-// Add event listener for the "Get Weather" button
-var button = document.querySelector("button[type='submit']");
-button.addEventListener("click", function() {
-  var city = document.getElementById("city").value;
-  api.getWeatherForecast(city, "5ec5845a9b7c8e9451636b9e096d36ab");
-});
 
